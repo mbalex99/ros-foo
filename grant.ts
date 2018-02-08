304a8351702873142ba99c095a5239545f18e262
Maximilian Alexander <mbalex99@gmail.com>
2018-02-07 16:31:48 -0800

    removing slashes

--- grant.ts
+++ grant.ts
@@ -4,7 +4,7 @@ const ROS_REALM_ENDPOINT = process.env.ROS_REALM_ENDPOINT || 'realm://127.0.0.1:
 const ROS_AUTH_ENDPOINT = process.env.ROS_AUTH_ENDPOINT ||'http://127.0.0.1:9080'
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'realm-admin';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || ''
-const REALM_PATH = process.env.REALM_PATH || 'foo'
+const REALM_PATH = process.env.REALM_PATH || '/foo'
 const USER_ID = process.env.USER_ID 
 
 async function main() {
