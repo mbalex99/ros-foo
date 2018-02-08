14de31fd4c0b6622e2d0c8ef42fd98602e928700
Maximilian Alexander <mbalex99@gmail.com>
2018-02-07 16:11:47 -0800

    adding readme

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 `npm install`
 `npm start`
+
+## Add runtime variables
+
+See `index.ts` for more info
+
+but you can run something like this
+
+`REALM_PATH=bar npm start`
--- index.ts
+++ index.ts
@@ -4,12 +4,13 @@ const ROS_REALM_ENDPOINT = process.env.ROS_REALM_ENDPOINT || 'realm://127.0.0.1:
 const ROS_AUTH_ENDPOINT = process.env.ROS_AUTH_ENDPOINT ||'http://127.0.0.1:9080'
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'realm-admin';
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || ''
+const REALM_PATH = process.env.REALM_PATH || '/foo'
 
 async function main() {
   const adminUser = await Realm.Sync.User.login(ROS_AUTH_ENDPOINT, ADMIN_USERNAME, ADMIN_PASSWORD)
   const realm = await Realm.open({
     sync: {
-      url: `${ROS_REALM_ENDPOINT}/foo`,
+      url: `${ROS_REALM_ENDPOINT}/${REALM_PATH}`,
       user: adminUser
     }
   })
